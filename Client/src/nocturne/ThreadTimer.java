f0fdd1914771aa6532783c3481e73ef049755967
JakeWestcott <32679007+JakeWestcott@users.noreply.github.com>
2018-04-25 20:15:43 +0100

    Created the thread timer class.

---
+++ Client/src/nocturne/ThreadTimer.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nocturne;
+
+/**
+ *
+ * @author jakew
+ */
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ThreadTimer extends Thread{
+    
+    private Home form;
+    private boolean Request;
+    
+    public ThreadTimer SetForm(Home screen)
+    {
+        this.form = screen;
+        ThreadTimer PassBack = new ThreadTimer();
+        return PassBack;
+    }
+    
+    public void SetRequest (boolean request)
+    {
+        this.Request = request;
+    }
+    
+    @Override
+    public void run() 
+    {
+        while(Request == true)
+        {
+            try {
+                  form.RefreshActiveFriendsList();
+                  form.RefreshFriendsRequestList();
+                  form.RefreshPosts();
+                  form.RefreshMySongs();
+                  form.RefreshAllFriendsList();
+                  Thread.sleep(2000);
+            } catch (IOException | ClassNotFoundException | InterruptedException ex) {
+                  Logger.getLogger(ThreadTimer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                    
+        }
+    }
+    
+}
